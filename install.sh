10c86199a603b655a7bc850e86ac1f7527234d6f
Ron Belaire <rbelaire@ciena.com>
2020-02-19 13:23:21 -0500

    diff-so-fancy work on Mac

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="ackrc bash_profile bashrc gvimrc inputrc screenrc vimrc xsession Xmodmap"
+files="ackrc bash_profile bashrc gitconfig gvimrc inputrc screenrc vimrc xsession Xmodmap"
 
 ##########
 
